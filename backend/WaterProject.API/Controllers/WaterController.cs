5627a523c00a8eb180a35dd9fdf099bf6c8dc842
michajones16 <michajones16@gmail.com>
2026-03-17 23:18:39 -0600

    Initial Commit

---
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.API.Data;
+
+namespace WaterProject.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDbContext _waterContext;
+        public WaterController(WaterDbContext temp) => _waterContext = temp;
+
+        [HttpGet("AllProjects")]
+        public IEnumerable<Project> Get()
+        {
+            var projects = _waterContext.Projects.ToList();
+
+            return projects;
+        }
+
+        [HttpGet("FunctionalProjects")]
+        public IEnumerable<Project> GetFunctionalProjects()
+        {
+            var projects = _waterContext.Projects.Where(x => x.ProjectFunctionalityStatus == "Functional").ToList();
+            return projects;
+        }
+    }
+}
